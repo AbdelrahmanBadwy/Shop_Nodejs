dce495d1fc2ed583264cc58bf1a2de87c31116f4
Abdelrahman Badawy <badawy@MacBook-Pro-Abdelrahman.local>
2023-07-06 08:05:47 +0300

    Error controller

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const errorController = require('./controllers/Error')
 const app = express(); // Express Makes The app {Server}
 
 // Adding Template Engine 
-// app.set('view engine','pug'); //Use pug template engine
 app.set('view engine','ejs');
-app.set('views','views'); //Our html files in folder named views
+app.set('views','views'); //Our ejs files in folder named views
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -21,7 +20,5 @@ app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 
-app.use((req,res,next)=>{
-   res.status(404).render('404',{pageTitle : '404 badawy'});
-});
+app.use(errorController.get404);
 app.listen(3000);
