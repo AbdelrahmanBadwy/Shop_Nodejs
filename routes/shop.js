0207e629874de4576e7957956424a6ce91018de7
Abdelrahman Badawy <badawy@MacBook-Pro-Abdelrahman.local>
2023-07-06 07:59:04 +0300

    Adding Controllers

--- routes/shop.js
+++ routes/shop.js
@@ -1,21 +1,9 @@
 
-
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const adminData = require('./admin');
+const productController = require('../controllers/products');
 
 
-router.get('/', (req, res, next) => {
-   const products = adminData.products;
-   res.render('shop', {
-     prods: products,
-     pageTitle: 'Shop',
-     path: '/',
-     hasProducts: products.length > 0,
-     activeShop: true,
-     productCSS: true
-   });
- });
+router.get('/',productController.getProducts);
 
 module.exports=router;
