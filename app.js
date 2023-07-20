d112d6eb690fc4c35f3a3c46c8257babf3aeb1a8
Abdelrahman Badawy <badawy@MacBook-Pro-Abdelrahman.local>
2023-07-20 11:21:03 +0300

    mongoose is done

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const path = require('path');
-const mongoConnect = require('./util/database').mongoConnect;
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -16,13 +18,13 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req ,res ,next)=>{
-    User.findById('64b6cab65db3581fc2fe42a8')
-        .then(user =>{
-            req.user = new User(user.name , user.email ,user._id,user.cart);
-            next();
-        })
-        .catch(err => console.log(err));
+app.use((req, res, next) => {
+  User.findById('64b6cab65db3581fc2fe42a8')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -30,8 +32,25 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-
-
-mongoConnect(() => {
+mongoose
+  .connect(
+    'mongodb+srv://abdalrmanbadwy:W7qp1OI3fl2kktYc@cluster0.z40ugzh.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
     app.listen(3000);
-});
+  })
+  .catch(err => {
+    console.log(err);
+  });
